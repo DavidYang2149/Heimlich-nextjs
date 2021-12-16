f8131d1e8853bc329d173ff8f5899c17bb0276c8
davidyang2149 <davidyang2149@gmail.com>
2021-12-16 21:59:58 +0900

    :white_check_mark: LactationPage 테스트코드에 Redux를 주입합니다
    prototype

--- __tests__/lactation/index.test.tsx
+++ __tests__/lactation/index.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
+import mockState, { mockUseDispatch, mockUseSelector } from '__mocks__/fixtures/mockTools';
 import LactationPage from 'pages/lactation/index';
+import { RootReducer } from 'src/redux/rootReducer';
+
+jest.mock('react-redux');
 
 describe('LactationPage', () => {
-  it('renders', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    dispatch.mockClear();
+    mockUseDispatch.mockImplementation(() => dispatch);
+    mockUseSelector.mockImplementation((selector: (arg: RootReducer) => void) => selector({
+      ...mockState,
+    }));
+  });
+
+  it('페이지를 랜딩합니다', () => {
     const { container } = render(<LactationPage />);
 
     expect(container).toHaveTextContent(/수유 일지/i);

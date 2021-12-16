953803cc03ca4c96018bbe3e569c4baee95a3268
davidyang2149 <davidyang2149@gmail.com>
2021-12-16 22:30:01 +0900

    :white_check_mark: E2E 수유입력 테스트 케이스를 추가합니다
    prototype

--- cypress/integration/insert.spec.js
+++ cypress/integration/insert.spec.js
@@ -1,7 +1,25 @@
 describe('입력 화면', () => {
-  it('메인 화면을 확인합니다', () => {
+  it('입력 화면을 확인합니다', () => {
     cy.visit('http://localhost:3000/lactation')
 
     cy.get('main > h1').contains(/수유 일지/i);
+  });
+
+  it('분유 버튼을 클릭합니다', () => {
+    cy.get('#breastMilk').should('be.checked').and('have.value', 'breastMilk')
+
+    cy.get('#PowderedBottleMilk').check();
+
+    cy.get('#breastMilk').should('not.be.checked').and('have.value', 'breastMilk')
+    cy.get('#PowderedBottleMilk').should('be.checked').and('have.value', 'PowderedBottleMilk')
+  });
+
+  it('수유 용량을 입력합니다', () => {
+    cy.get('#amount').should('have.value', 0);
+
+    cy.get('#amount').type('{backspace}')
+    cy.get('#amount').type(40);
+
+    cy.get('#amount').should('have.value', 40);
   });
 });

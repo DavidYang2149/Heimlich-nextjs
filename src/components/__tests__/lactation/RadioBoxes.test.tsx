14df5176b9ed94ce7e00437dd760468723d50a4d
davidyang2149 <davidyang2149@gmail.com>
2021-12-19 16:19:54 +0900

    :white_check_mark: RadioBoxes 테스트코드를 작성합니다
    refactoring

---
+++ src/components/__tests__/lactation/RadioBoxes.test.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import RadioBoxes, { Option, Props } from 'src/components/lactation/RadioBoxes';
+
+describe('RadioBoxes', () => {
+  const handleChange = jest.fn();
+
+  beforeEach(() => {
+    handleChange.mockClear();
+  });
+
+  const renderRadioBoxes = ({
+    name, options, value, onChange,
+  }: Props) => render((
+    <RadioBoxes
+      name={name}
+      options={options}
+      value={value}
+      onChange={onChange}
+    />
+  ));
+
+  it('RadioBoxes 컴포넌트를 랜딩합니다', () => {
+    const options: Option[] = [
+      { title: '직수', lactation: 'breastMilk' },
+      { title: '모유', lactation: 'MotherBottleMilk' },
+      { title: '분유', lactation: 'PowderedBottleMilk' },
+    ];
+
+    const { getByLabelText } = renderRadioBoxes({
+      name: 'lactationType',
+      options,
+      value: 'breastMilk',
+      onChange: handleChange,
+    });
+
+    expect(getByLabelText('직수')).toBeInTheDocument();
+    expect(getByLabelText('모유')).toBeInTheDocument();
+    expect(getByLabelText('분유')).toBeInTheDocument();
+  });
+
+  context('라디오 버튼을 클릭을 클릭하면', () => {
+    it('onChange 함수를 실행합니다', () => {
+      const options: Option[] = [
+        { title: '직수', lactation: 'breastMilk' },
+        { title: '모유', lactation: 'MotherBottleMilk' },
+        { title: '분유', lactation: 'PowderedBottleMilk' },
+      ];
+
+      const { getByLabelText } = renderRadioBoxes({
+        name: 'lactationType',
+        options,
+        value: 'breastMilk',
+        onChange: handleChange,
+      });
+
+      const radioButton = getByLabelText('분유');
+      fireEvent.click(radioButton);
+
+      expect(handleChange).toBeCalled();
+    });
+  });
+});

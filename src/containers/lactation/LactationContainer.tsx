7d96fc226e1a6f741b442e7ec19ab68667ae87ed
davidyang2149 <davidyang2149@gmail.com>
2021-12-19 16:23:29 +0900

    :hammer: Lactation 컨테이너를 리팩터링합니다
    refactoring
    - JSX를 컴포넌트로 추출

--- src/containers/lactation/LactationContainer.tsx
+++ src/containers/lactation/LactationContainer.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
+import InputBox from 'src/components/lactation/InputBox';
+import RadioBoxes, { Option } from 'src/components/lactation/RadioBoxes';
 import { setInteraction } from 'src/redux/lactation/interaction';
 import { RootReducer } from 'src/redux/rootReducer';
 
@@ -14,6 +16,11 @@ const LactationContainer = () => {
   }));
 
   const { lactationType, amount } = interaction;
+  const options: Option[] = [
+    { title: '직수', lactation: 'breastMilk' },
+    { title: '모유', lactation: 'MotherBottleMilk' },
+    { title: '분유', lactation: 'PowderedBottleMilk' },
+  ];
 
   const handleInteractionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -25,48 +32,18 @@ const LactationContainer = () => {
       <h1 className={styles.title}>
         수유 일지
       </h1>
-      <div>
-        <label htmlFor="breastMilk" className={styles.label}>직수</label>
-        <input
-          type="radio"
-          name="lactationType"
-          id="breastMilk"
-          value="breastMilk"
-          onChange={handleInteractionChange}
-          checked={lactationType === 'breastMilk'}
-        />
-        <label htmlFor="MotherBottleMilk" className={styles.label}>모유</label>
-        <input
-          type="radio"
-          name="lactationType"
-          id="MotherBottleMilk"
-          value="MotherBottleMilk"
-          onChange={handleInteractionChange}
-          checked={lactationType === 'MotherBottleMilk'}
-        />
-        <label htmlFor="PowderedBottleMilk" className={styles.label}>분유</label>
-        <input
-          type="radio"
-          name="lactationType"
-          id="PowderedBottleMilk"
-          value="PowderedBottleMilk"
-          onChange={handleInteractionChange}
-          checked={lactationType === 'PowderedBottleMilk'}
-        />
-      </div>
-      <div>
-        <label htmlFor="amount" className={styles.label}>용량</label>
-        <input
-          type="number"
-          id="amount"
-          name="amount"
-          value={amount}
-          min="0"
-          max="999"
-          className={styles.amount}
-          onChange={handleInteractionChange}
-        />
-      </div>
+      <RadioBoxes
+        name="lactationType"
+        options={options}
+        value={lactationType}
+        onChange={handleInteractionChange}
+      />
+      <InputBox
+        id="amount"
+        title="용량"
+        value={amount}
+        onChange={handleInteractionChange}
+      />
       <div>
         <button type="button" className={styles.button}>저장</button>
       </div>

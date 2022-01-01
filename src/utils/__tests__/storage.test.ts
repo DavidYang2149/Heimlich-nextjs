76e45fe5be6040d254ff3f7ccb44a1af0ab25bdc
davidyang2149 <davidyang2149@gmail.com>
2022-01-01 14:37:32 +0900

    :hammer: 로컬 저장소를 생성합니다
    local storage

---
+++ src/utils/__tests__/storage.test.ts
@@ -0,0 +1,49 @@
+import {
+  saveItem, loadItem, removeItem, clearItem,
+} from 'src/utils/storage';
+
+describe('storage', () => {
+  jest.spyOn(window.localStorage.__proto__, 'getItem');
+  jest.spyOn(window.localStorage.__proto__, 'setItem');
+  jest.spyOn(window.localStorage.__proto__, 'removeItem');
+  jest.spyOn(window.localStorage.__proto__, 'clear');
+
+  beforeEach(() => {
+    Storage.prototype.setItem = jest.fn();
+    Storage.prototype.getItem = jest.fn();
+    Storage.prototype.removeItem = jest.fn();
+    Storage.prototype.clear = jest.fn();
+  });
+
+  describe('saveItem', () => {
+    describe('loadItem', () => {
+      it('localStorage의 getItem 함수를 호출합니다', () => {
+        loadItem({ key: 'flower' });
+
+        expect(localStorage.getItem).toBeCalledWith('flower');
+      });
+    });
+
+    it('localStorage의 setItem 함수를 호출합니다', () => {
+      saveItem({ key: 'password', value: 'hackerman' });
+
+      expect(localStorage.setItem).toBeCalledWith('password', 'hackerman');
+    });
+  });
+
+  describe('removeItem', () => {
+    it('localStorage의 removeItem 함수를 호출합니다', () => {
+      removeItem({ key: 'sky' });
+
+      expect(localStorage.removeItem).toBeCalledWith('sky');
+    });
+  });
+
+  describe('clearItem', () => {
+    it('localStorage의 clear 함수를 호출합니다', () => {
+      clearItem();
+
+      expect(localStorage.clear).toBeCalled();
+    });
+  });
+});

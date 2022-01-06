a336557f7fd84994feeb785ca10a19f58a2a7ed8
davidyang2149 <davidyang2149@gmail.com>
2022-01-06 13:01:43 +0900

    :hammer: 최근 수유 기록을 표시합니다
    prototype

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -25,4 +25,34 @@ describe('Home', () => {
 
     expect(container).toHaveTextContent(/수유 기록하기/i);
   });
+
+  context('수유 기록이 존재하면', () => {
+    it('마지막 수유 시간을 표시합니다', () => {
+      mockUseSelector.mockImplementation((selector: (arg: RootState) => void) => selector({
+        ...mockState,
+        record: {
+          records: [
+            {
+              lactationType: 'PowderedBottleMilk',
+              amount: 80,
+              recordTime: '2022-01-04T12:34:14.809Z',
+            },
+          ],
+        },
+      }));
+
+      const { container } = render(<Home />);
+
+      expect(container).toHaveTextContent(/마지막 수유 시간/i);
+      expect(container).toHaveTextContent(/2022-01-04 12:34/i);
+    });
+  });
+
+  context('수유 기록이 존재하지 않으면', () => {
+    it('마지막 수유 시간이 표시되지 않습니다', () => {
+      const { container } = render(<Home />);
+
+      expect(container).not.toHaveTextContent(/마지막 수유 시간/i);
+    });
+  });
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,10 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { loadRecords } from 'src/redux/lactation/record';
+import { RootState } from 'src/redux/rootReducer';
 
 import styles from 'styles/Home.module.css';
 
@@ -15,6 +16,13 @@ const Home: NextPage = () => {
   useEffect(() => {
     dispatch(loadRecords());
   }, [dispatch]);
+
+  const { record } = useSelector((state: RootState) => ({
+    record: state.record,
+  }));
+
+  const { records } = record;
+  const recentRecord = (records.length > 0 && records[0]) || null;
 
   return (
     <div className={styles.container}>
@@ -28,6 +36,15 @@ const Home: NextPage = () => {
         <figure className={styles.image}>
           <Image src="/assets/unicorn.svg" alt="unicorn Logo" width={200} height={200} />
         </figure>
+        {
+          recentRecord && (
+            <h2>
+              마지막 수유 시간:
+              {' '}
+              {recentRecord.recordTime.replace('T', ' ').substring(0, 16)}
+            </h2>
+          )
+        }
         <h1 className={styles.title}>
           <Link href="/lactation">
             수유 기록하기

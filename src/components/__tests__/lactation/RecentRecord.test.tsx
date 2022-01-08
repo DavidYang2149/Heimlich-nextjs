fd88340bb63f7a7a97fe35a913599edbd82d3946
davidyang2149 <davidyang2149@gmail.com>
2022-01-08 16:30:56 +0900

    :recycle: 테스트코드의 변수명을 명확하게 리팩토링합니다
    refactoring

--- src/components/__tests__/lactation/RecentRecord.test.tsx
+++ src/components/__tests__/lactation/RecentRecord.test.tsx
@@ -7,12 +7,13 @@ import { Lactation } from 'src/types/lactation';
 describe('RecentRecord', () => {
   context('가장 최근 수유 기록을 입력하면', () => {
     it('수유 기록을 표시합니다', () => {
-      const record: Lactation = {
+      const recentRecord: Lactation = {
         lactationType: 'MotherBottleMilk',
         amount: 40,
         recordTime: '2022-01-04T12:34:42.062Z',
       };
-      const { container } = render((<RecentRecord record={record} />));
+
+      const { container } = render((<RecentRecord record={recentRecord} />));
 
       expect(container).toHaveTextContent('마지막 수유 시간');
     });
